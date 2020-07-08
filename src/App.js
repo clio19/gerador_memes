ed1c732eacd6c0c856269d8cc2939c341998c15e
htrix <>
2020-07-08 23:38:01 +0100

    🌍 Added meme lines

--- src/App.js
+++ src/App.js
@@ -1,24 +1,93 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import axios from 'axios'
+import Meme from './components/Meme';
+
+const objectToQueryParam = obj => {
+  const params = Object.entries(obj).map(([key, value]) => `${key}=${value}`);
+  return "?" + params.join("&");
+};
+
+
 
 function App() {
+
+  const [templates, setTemplates] = useState([]);
+  const [template, setTemplate] = useState(null);
+  const [topText, setTopText] = useState("");
+  const [bottomText, setBottomText] = useState("");
+  const [meme, setMeme] = useState(null);
+
+  useEffect(() => {
+    axios.get('https://api.imgflip.com/get_memes')
+      .then(response => {
+        // console.log("templates ", response.data.data.memes);
+        setTemplates(response.data.data.memes)
+      })
+      .catch(function (error) {
+        // handle error
+        console.log(error);
+      })
+  }, [])
+  if (meme) {
+    return (
+      <div style={{ textAlign: "center" }}>
+        <img style={{ width: 200 }} src={meme} alt="custom meme" />
+      </div>
+    );
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      {template && (
+        <form
+          onSubmit={async e => {
+            e.preventDefault()
+            // logica
+            const params = {
+              template_id: template.id,
+              text0: topText,
+              text1: bottomText,
+              username: process.env.username, // put this on .env file
+              password: process.env.password // put this on .env file
+            };
+
+            const response = await axios.get(`https://api.imgflip.com/caption_image${objectToQueryParam(params)}`)
+
+            const json = await response.data;
+            setMeme(json.data.url);
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          <Meme template={template} />
+          <input
+            placeholder="Texto Superior"
+            value={topText}
+            onChange={e => setTopText(e.target.value)}
+          />
+          <input
+            placeholder="bottom text"
+            value={bottomText}
+            onChange={e => setBottomText(e.target.value)}
+          />
+          <button type="submit">create meme</button>
+        </form>
+      )}
+      {!template && (
+        <>
+          <h1>Escolha uma Imagem</h1>
+          {templates.map(template => {
+            return (
+              <Meme
+                key={template.id}
+                template={template}
+                onClick={() => {
+                  setTemplate(template)
+                }}
+              />
+            )
+          })}
+        </>
+      )}
     </div>
   );
 }
---
+++ src/components/Meme.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+
+
+const Meme = ({ template, onClick }) => {
+    return (
+        <img
+            style={{ width: 200 }}
+            key={template.id}
+            src={template.url}
+            alt={template.name}
+            onClick={onClick}
+        />
+    )
+}
+
+
+export default Meme
